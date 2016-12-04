aa248ad6f6364683de7cc70fa6c3d6d5d611892c
Paul Hempshall <paul.hempshall@p-a-u-l.com>
2016-12-04 20:03:38 +0000

    Updates: - add request type and reponse status - add response time taken

--- chrome/fetchHeaders.js
+++ chrome/fetchHeaders.js
@@ -7,17 +7,17 @@ var filters = {
   types: ["main_frame"]
 };
 
-/* headers received */
-chrome.webRequest.onHeadersReceived.addListener(function(details) {
-  headers[details.tabId] = headers[details.tabId] || {};
-  headers[details.tabId].response = details;
-}, filters, ["responseHeaders"]);
-
 /* headers sent */
 chrome.webRequest.onSendHeaders.addListener(function(details) {
   headers[details.tabId] = headers[details.tabId] || {};
   headers[details.tabId].request = details;
 }, filters, ["requestHeaders"]);
+
+/* headers received */
+chrome.webRequest.onHeadersReceived.addListener(function(details) {
+  headers[details.tabId] = headers[details.tabId] || {};
+  headers[details.tabId].response = details;
+}, filters, ["responseHeaders"]);
 
 /* remove tab data from headers object when tab is onRemoved */
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
--- chrome/headersPopup.js
+++ chrome/headersPopup.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* this needs cleaning up :'( */
 chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
   var results = document.getElementById('results'),
       headers = chrome.extension.getBackgroundPage().headers[tab[0].id];
@@ -16,21 +17,51 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
 
   function printResults () {
     Object.keys(headers).forEach(function(key) {
- console.log(headers[key]);
+ // console.log(headers[key]);
       headers[key][key + 'Headers'].sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);});
 
       printHeading(key);
 
       for (var i = 0; i < headers[key][key + 'Headers'].length; i++) {
+      	if (i === 0) {
+      		printStatus(headers[key]);
+      	}
         printHeader(headers[key][key + 'Headers'][i]);
       }
     });
+
+    function printStatus (obj) {
+      var p = document.createElement('p'),
+          b = document.createElement('b');
+
+      if (obj.statusLine) {
+      	b.appendChild(document.createTextNode(obj.statusLine));
+      }
+      else {
+      	b.appendChild(document.createTextNode(obj.method + ' ' + obj.url));
+      }
+      p.appendChild(b);
+      results.appendChild(p);
+    }
+
+    function calcTime () {
+			return ((headers['response'].timeStamp - headers['request'].timeStamp) / 1000).toFixed(4);
+    }
 
     function printHeading (key) {
       var n = key[0].toUpperCase() + key.substring(1);
       var h = document.createElement('h2'),
           t = document.createTextNode(n);
+
       h.appendChild(t);
+
+      if (key === 'response') {
+      	var timeTaken = calcTime(),
+      			s = document.createElement('small');
+      	s.appendChild(document.createTextNode(' (in ' + timeTaken + 's)'));
+      	h.appendChild(s);
+      }
+
       results.appendChild(h);
     }
 
