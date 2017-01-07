fbdcdec29e1d7912efc39fb265754e1f96d3bea1
Paul Hempshall <paul.hempshall@p-a-u-l.com>
2017-01-07 14:36:01 +0000

    clean up code

--- chrome/headersPopup.js
+++ chrome/headersPopup.js
@@ -1,88 +1,77 @@
 'use strict';
 
-/* this needs cleaning up :'( */
 chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
   var results = document.getElementById('results'),
       headers = chrome.extension.getBackgroundPage().headers[tab[0].id];
-
-  function printError () {
-    var error = {
-      container: document.createElement('p'),
-      text: document.createTextNode("Error: could not get http headers, please try refreshing the page.")
-    }
-    error.container.className = "error-text";
-    error.container.appendChild(error.text)
-    results.appendChild(error.container);
-  }
-
-  function printResults () {
-    Object.keys(headers).forEach(function(key) {
- // console.log(headers[key]);
-      headers[key][key + 'Headers'].sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);});
-
-      printHeading(key);
-
-      for (var i = 0; i < headers[key][key + 'Headers'].length; i++) {
-      	if (i === 0) {
-      		printStatus(headers[key]);
-      	}
-        printHeader(headers[key][key + 'Headers'][i]);
-      }
-    });
-
-    function printStatus (obj) {
-      var p = document.createElement('p'),
-          b = document.createElement('b');
-
-      if (obj.statusLine) {
-      	b.appendChild(document.createTextNode(obj.statusLine));
-      }
-      else {
-      	b.appendChild(document.createTextNode(obj.method + ' ' + obj.url));
-      }
-      p.appendChild(b);
-      results.appendChild(p);
-    }
-
-    function calcTime () {
-			return ((headers['response'].timeStamp - headers['request'].timeStamp) / 1000).toFixed(4);
-    }
-
-    function printHeading (key) {
-      var n = key[0].toUpperCase() + key.substring(1);
-      var h = document.createElement('h2'),
-          t = document.createTextNode(n);
-
-      h.appendChild(t);
-
-      if (key === 'response') {
-      	var timeTaken = calcTime(),
-      			s = document.createElement('small');
-      	s.appendChild(document.createTextNode(' (in ' + timeTaken + 's)'));
-      	h.appendChild(s);
-      }
-
-      results.appendChild(h);
-    }
-
-    function printHeader (obj) {
-      var p = document.createElement('p'),
-          b = document.createElement('b'),
-          t_name = document.createTextNode(obj.name + ': '),
-          t_value = document.createTextNode(obj.value);
-      b.appendChild(t_name);
-
-      p.appendChild(b);
-      p.appendChild(t_value);
-      results.appendChild(p);
-    }
-  }
-
 
   if (headers === undefined) {
     printError();
   }
   else {
     printResults();
+  }
+
+
+  function clearResults () {
+    results.innerHTML = '';
+  }
+
+  function printError () {
+    var error = "Error: could not get http headers, please try refreshing the page.";
+
+    clearResults();
+    results.innerHTML += "<p class=\"error-text\">" + error + "</p>"
+  }
+
+  function printResults () {
+    clearResults();
+
+    // print request
+    headers['request']['requestHeaders'].sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);});
+    printHeading('request');
+    printKeys('request');
+
+    // print response
+    headers['response']['responseHeaders'].sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);});
+    printHeading('response');
+    printKeys('response');
+
+    
+    function calcTime () {
+      return ((headers['response'].timeStamp - headers['request'].timeStamp) / 1000).toFixed(4);
+    }
+
+    function printHeading (key) {
+      var t = key[0].toUpperCase() + key.substring(1);
+
+      if (key === 'response') {
+        results.innerHTML += "<h2>" + t + " <small>(in " + calcTime() + "s)</small></h2>";
+      }
+      else {
+        results.innerHTML += "<h2>" + t + "</h2>";
+      }
+    }
+
+    function printStatus (obj) {
+      if (obj.statusLine) {
+        results.innerHTML += "<p><b>" + obj.statusLine + "</b></p>";
+      }
+      else {
+        results.innerHTML += "<p><b>" + obj.method + " " + obj.url + "</b></p>";
+      }
+    }
+
+    function printHeader (obj) {
+      results.innerHTML += "<p><b>" + obj.name + ":</b> " + obj.value + "</p>";
+    }
+
+    function printKeys (key) {
+      for (var i = 0; i < headers[key][key + 'Headers'].length; i++) {
+        if (i === 0) {
+          printStatus(headers[key]);
+        }
+        printHeader(headers[key][key + 'Headers'][i]);
+      }
+    }
   }
 });
